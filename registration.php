a808c3a913fd4f8057eac4eac50b5a46a2dded1a
Watch3ruk <48841019+Watch3ruk@users.noreply.github.com>
2023-07-19 10:31:20 +0100

    Update registration.php

--- registration.php
+++ registration.php
@@ -2,6 +2,6 @@
 
 \Magento\Framework\Component\ComponentRegistrar::register(
      \Magento\Framework\Component\ComponentRegistrar::MODULE,
-     'Watch3ruk_AutoInStock',
+     'Watch3r_AutoInStock',
     __DIR__
 );

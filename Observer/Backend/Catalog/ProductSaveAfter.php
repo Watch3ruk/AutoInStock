7cc7a8770d0acf20153c5df9fc7da413c7bfae01
Watch3ruk <48841019+Watch3ruk@users.noreply.github.com>
2023-07-19 10:16:57 +0100

    Update ProductSaveAfter.php

--- Observer/Backend/Catalog/ProductSaveAfter.php
+++ Observer/Backend/Catalog/ProductSaveAfter.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 
-namespace Watch3ruk\AutoInStock\Observer\Backend\Catalog;
+namespace watch3ruk\module-autoinstock\Observer\Backend\Catalog;
 
 class ProductSaveAfter implements \Magento\Framework\Event\ObserverInterface
 {
